2741f0cc08f2699903fe423c37711c320976ad25
Jothiprakash-8012 <jothiprakash422@gmail.com>
2025-07-18 12:47:14 +0530

    Add files via upload

---
+++ Program3.java
@@ -0,0 +1,20 @@
+package myfirstproject;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=15,b=25,c=05;
+if(a>b) {
+	System.out.println("a is greater than:"+a);
+	
+}
+else if(b>c) {
+	System.out.println("b is greater than:"+b);
+}
+else {
+	System.out.println("c is less than value:"+c);
+}
+	}
+
+}
